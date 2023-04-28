d953bd376a2a5d643a5e645e095c072fb3defe21
Qmun14 <mamunramdhan@gmail.com>
2023-04-28 22:18:41 +0700

    Multiple element testing | dengan contoh sebuah component sidebar yang biasanya memiliki lebih dari 1 element atribut  pada componen-nya

---
+++ src/SiderBar.tsx
@@ -0,0 +1,21 @@
+
+const SideBar = ({ items }: {
+  items: {
+    name: string;
+    href: string;
+  }[];
+}) => {
+  return (
+    <div>
+      {
+        items.map((item) => (
+          <div key={item.href}>
+            <a role="navigation" href={item.href}>{item.name}</a>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default SideBar;
